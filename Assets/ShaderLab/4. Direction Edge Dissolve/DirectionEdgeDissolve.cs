e75c9c58bf5e296cb271b88566c3150e6c3a23be
EGLIK <eglik@odin.vision>
2019-05-26 03:08:23 +0900

    new: Direction Edge Dissolve.shader

---
+++ Assets/ShaderLab/4. Direction Edge Dissolve/DirectionEdgeDissolve.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionEdgeDissolve : MonoBehaviour
+{
+    [SerializeField]
+    private float minValue;
+    
+    [SerializeField]
+    private float maxValue;
+
+    private float value;
+
+    [SerializeField]
+    private float timer;
+
+    [SerializeField]
+    private float speed;
+
+    void Update()
+    {
+        timer += Time.deltaTime * speed;
+
+        GetComponent<Renderer>().material.SetFloat("_Value", Mathf.Lerp(minValue, maxValue, Mathf.Repeat(timer, 1)));
+        Debug.Log(GetComponent<Renderer>().material.GetFloat("_Value"));
+    }
+}
